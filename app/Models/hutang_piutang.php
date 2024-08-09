11be5786f6c691e148f6677f057af0663f5eb4cd
Awie-chitato <awkanzha@gmail.com>
2024-08-09 15:10:13 +0700

    first commit

--- app/Models/hutang_piutang.php
+++ app/Models/hutang_piutang.php
@@ -5,7 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class hutang_piutang extends Model
-{
+   { 
     use HasFactory;
+
+    protected $fillable=[
+        'nama_event',
+        'jatuh_tempo_hutang_piutang',
+        'pihak_kedua_hutang_piutang',
+        'nominal_hutang_piutang',
+        'status_hutang_piutang'];
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EvenController;
-
+use App\Http\Controllers\HutangPiutangController;
 
 Route::get('/get-event',[EvenController::class, 'getEvents']);
 Route::get('/get-detail-event/{id}',[EvenController::class, 'getEvent']);
@@ -12,3 +12,8 @@ Route::post('/update-event/{id}',[EvenController::class, 'updateEvent']);
 Route::delete('/delete-event/{id}',[EvenController::class, 'deleteEvent']);
 
 
+Route::get('/get-hutang-piutang',[HutangPiutangController::class, 'getHutangPiutang']);
+Route::get('/get-detail-hutang-piutang/{id}',[HutangPiutangController::class, 'getHutangPiutang']);
+Route::post('/create-hutang-piutang',[HutangPiutangController::class, 'createHutangPiutang']);
+Route::post('/update-hutang-piutang/{id}',[HutangPiutangController::class, 'updateHutangPiutang']);
+Route::delete('/delete-hutang-piutang/{id}',[HutangPiutangController::class, 'deleteHutangPiutang']);
