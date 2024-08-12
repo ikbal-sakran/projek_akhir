a06cd4f634d164c3e21ee7213abf5521b1a72f9c
ikbal-sakran <firmansyahikbal89@gmail.com>
2024-08-12 10:32:37 +0700

    prepare merge

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EvenController;
-
+use App\Http\Controllers\TransaksiController;
 
 Route::get('/get-event',[EvenController::class, 'getEvents']);
 Route::get('/get-detail-event/{id}',[EvenController::class, 'getEvent']);
@@ -11,4 +11,6 @@ Route::post('/create-event',[EvenController::class, 'createEvent']);
 Route::post('/update-event/{id}',[EvenController::class, 'updateEvent']);
 Route::delete('/delete-event/{id}',[EvenController::class, 'deleteEvent']);
 
-
+Route::post('/create-transaksi', [TransaksiController::class,'createTransaksi']);
+Route::post('/update-transaksi/{id}', [TransaksiController::class,'updateTransaksi']);
+Route::delete('/delete-transaksi/{id}', [TransaksiController::class,'deleteTransaksi']);
